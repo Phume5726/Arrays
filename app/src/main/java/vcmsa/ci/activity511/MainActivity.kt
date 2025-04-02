31522f6a868b87e7a6185a2e95ba5c7b8616e7cc
Phume5726 <phumelelamathe49@gmail.com>
2025-04-02 23:02:53 +0200

    First Commit

---
+++ app/src/main/java/vcmsa/ci/activity511/MainActivity.kt
@@ -0,0 +1,35 @@
+package vcmsa.ci.activity511
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+
+    var edtNumber = findViewById<EditText>(R.id.edtNumber)
+    var btnSave = findViewById<Button>(R.id.btnSave)
+    var arrNumber = ArrayList<Int>(10)
+    var txtCount = findViewById<TextView>(R.id.txtCount)
+
+    btnSave.setOnClickListener
+    {
+
+    }
+
+}
+}
